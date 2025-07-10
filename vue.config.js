b9a597e585ae46b81ff0aaffcbd989a9cb30b76c
tuantrung128 <tuantrunglc@gmail.com>
2025-07-10 22:08:37 +0700

    update code

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-module.exports = {
-  publicPath: '/new-demo/' // <--- tên repo của bạn
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+// module.exports = {
+//   publicPath: '/new-demo/' // <--- tên repo của bạn
+// }
